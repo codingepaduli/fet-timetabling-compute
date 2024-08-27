8aab8d9b539d77bea88121d336eaa33595fbd9eb
codingepaduli <padulodeveloper@protonmail.com>
2024-08-27 19:30:01 +0200

    Move functions about template utils in a specific file

--- libs/aggiungiExtraAula.js
+++ libs/aggiungiExtraAula.js
@@ -70,41 +70,6 @@ function createTable(auleArray) {
 
 }
 
-function addTemplate(targetId, templateID) {
-  if (typeof targetId !== 'string' && ! (targetId instanceof String) ) {
-    throw new Error("param 'targetId' is not a string");
-  }
-  if (typeof templateID !== 'string' && ! (templateID instanceof String) ) {
-    throw new Error("param 'templateID' is not a string");
-  }
-  
-  const target = document.querySelector(targetId);
-  const template = document.querySelector(templateID);
-  
-  if (target != null && template != null) {
-    target.append(template.content.cloneNode(true));
-  }
-}
-
-function replaceTemplateData(dataNodeSelector, dataToReplace={}) {
-  if (typeof dataNodeSelector !== 'string' && ! (dataNodeSelector instanceof String) ) {
-    throw new Error("param 'dataNodeSelector' is not a string");
-  }
-  if (!dataToReplace instanceof Object || Array.isArray(dataToReplace) || dataToReplace === null) {
-    throw new Error("param 'dataToReplace' is not an object (empty object is allowed)");
-  }
-
-  const nodeToReplace = document.querySelectorAll(dataNodeSelector + " .replaceData");
-  console.debug(`Found ${nodeToReplace.length} elements to replace in node ${dataNodeSelector}`);
-
-  nodeToReplace.forEach(node => {
-    Object.entries(dataToReplace).forEach(([key, val]) => {
-      console.debug(`Replacing \$\{${key}\} --> ${val}`);
-      node.textContent = node.textContent.replace(`\$\{${key}\}` , `${val}`);
-    });
-  });
-}
-
 function showRoom(index, aula) {
   console.log(aula);
   aula.Classe = "6C_Tlc"
